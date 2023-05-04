185a6ebe498a14b293a8d90353328d0c1b8278b2
Nonya Johnson III, Esquire <nonya@business.com>
2023-05-03 21:32:15 -0500

    Made sql select queries more explicit and added new field

--- smwc/sql/select_hacks.sql
+++ smwc/sql/select_hacks.sql
@@ -1,7 +1,11 @@
-SELECT h.*,
+SELECT h.id, h.title, h.created_on, h.page_url, 
+    h.is_demo, h.is_featured, h.exit_count, 
+    h.rating, h.size, h.size_units,
+    h.download_url, h.downloaded_count,
     GROUP_CONCAT(DISTINCT ht.type) as types,
     GROUP_CONCAT(DISTINCT ha.author) as authors,
-    GROUP_CONCAT(DISTINCT hp.path) as paths
+    GROUP_CONCAT(DISTINCT hp.path) as paths,
+    h.exits_cleared
 FROM hacks h
 LEFT JOIN hack_types ht ON ht.hack_id = h.id
 LEFT JOIN hack_authors ha ON ha.hack_id = h.id
